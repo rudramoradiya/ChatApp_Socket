e1ee7f9e0e8015f866d9061491b50202951146f1
rudra moradiya <rudramoradiya123@gmail.com>
2026-06-19 23:33:50 +0530

    change frontend url

--- ChatApp_Backend/src/server.js
+++ ChatApp_Backend/src/server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://chat-app-socket-ten.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-socket-ten.vercel.app/",
     methods: ["GET", "POST"],
     credentials: true, 
   },
